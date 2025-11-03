2be53740576e32141d2a304d499c468b5f68c68f
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2025-11-03 00:11:36 -0600

    Add files via upload

---
+++ Actividades/Po02_variables.py
@@ -0,0 +1,7 @@
+print('Inicio',"del",'programa')
+
+variable = 15
+print(variable)
+
+miVariable = "Pedro"
+print('Su nombre es', miVariable)
---
+++ Actividades/Po03_Tiposdedatos.py
@@ -0,0 +1,63 @@
+# str, string o cadena de caracteres
+
+cadenaDeCaracteres = 'Hola a todos'
+
+print (f'{cadenaDeCaracteres=} | Tipo: {type(cadenaDeCaracteres)}')
+
+#Caracter
+
+caracter= "$"
+print(f'{caracter=} | Tipo: {type(caracter)}')
+
+
+#Caracter nulo o vacio
+caracterVacio =''
+print(f'{caracterVacio=} | Tipo: {type(caracterVacio)}')
+
+
+#numero o entero
+
+numeroEntero =666
+print(f'{numeroEntero=} | Tipo: {type(numeroEntero)}')
+
+#float
+
+numerofloat =3.1416
+print(f'{numerofloat=} | Tipo: {type(numerofloat)}')
+
+#booleano
+booleano = False
+print(f'{booleano=} | Tipo: {type(booleano)}')
+
+#nulo
+nulo= None
+print(f'{nulo=} | Tipo: {type(nulo)}')
+
+#DATOS NO PRIMITIVOS
+
+
+
+# -->LISTA
+
+lista=[2,5,8]
+print(f'{lista=} | Tipo: {type(lista)}')
+
+
+
+#tupla
+
+tupla=['a','c','e']
+print(f'{tupla=} | Tipo: {type(tupla)}')
+
+
+#conjunto
+
+conjunto=['Pedro','Marcos','David']
+print(f'{conjunto=} | Tipo: {type(conjunto)}')
+
+
+#Diccionario
+diccionario ={'llave1':True,'llave2':15.89}
+print(f'{diccionario=} | Tipo: {type(diccionario)}')
+
+
